dc1141ab99e81c7906aab18b779f22d24385f62a
Yiunam LI <liyiunam37@gmail.com>
2026-01-17 18:33:12 +0800

    added m 80

--- leetcode_java/app/src/main/java/org/example/app/Medium80.java
+++ leetcode_java/app/src/main/java/org/example/app/Medium80.java
@@ -2,7 +2,9 @@ package org.example.app;
 
 public class Medium80 {
     public int removeDuplicates(int[] nums) {
+
         return 0;
+
     }
 
 }
--- leetcode_java/app/src/test/java/org/example/app/TestM80.java
+++ leetcode_java/app/src/test/java/org/example/app/TestM80.java
@@ -1,10 +1,25 @@
 package org.example.app;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class TestM80 {
     @Test
     void case1() {
+        Medium80 inst = new Medium80();
+        int[] nums = { 1, 1, 1, 2, 2, 3 };
+        int k = inst.removeDuplicates(nums);
 
+        assertEquals(5, k);
+
+    }
+
+    @Test
+    void case2() {
+        int[] nums = { 0, 0, 1, 1, 1, 1, 2, 3, 3 };
+        Medium80 inst = new Medium80();
+        int k = inst.removeDuplicates(nums);
+        assertEquals(7, k);
     }
 }
