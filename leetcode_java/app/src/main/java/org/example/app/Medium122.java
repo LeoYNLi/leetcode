d8565b94a2f641b024c560a862c5283d73ea5031
Yiunam LI <liyiunam37@gmail.com>
2026-01-19 11:49:21 +0800

    added m 122

---
+++ leetcode_java/app/src/main/java/org/example/app/Medium122.java
@@ -0,0 +1,9 @@
+package org.example.app;
+
+public class Medium122 {
+    public int maxProfit(int[] prices) {
+
+        return 0;
+
+    }
+}
---
+++ leetcode_java/app/src/test/java/org/example/app/TestM122.java
@@ -0,0 +1,31 @@
+package org.example.app;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestM122 {
+    @Test
+    void case1() {
+        Medium122 inst = new Medium122();
+        int[] prices = { 7, 1, 5, 3, 6, 4 };
+        int k = inst.maxProfit(prices);
+        assertEquals(7, k);
+    }
+
+    @Test
+    void case2() {
+        Medium122 inst = new Medium122();
+        int[] prices = { 1, 2, 3, 4, 5 };
+        int k = inst.maxProfit(prices);
+        assertEquals(4, k);
+    }
+
+    @Test
+    void case3() {
+        Medium122 inst = new Medium122();
+        int[] prices = { 7, 6, 4, 3, 1 };
+        int k = inst.maxProfit(prices);
+        assertEquals(0, k);
+    }
+}
