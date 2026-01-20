32d17bee1e3bc804116ce17b3bae8f1f3239263f
Yiunam LI <liyiunam37@gmail.com>
2026-01-20 22:47:41 +0800

    solved m 55

--- leetcode_java/app/src/main/java/org/example/app/Medium55.java
+++ leetcode_java/app/src/main/java/org/example/app/Medium55.java
@@ -2,36 +2,6 @@ package org.example.app;
 
 public class Medium55 {
     public boolean canJump(int[] nums) {
-
-        if (nums.length == 1){
-            return true;
-        }
-        boolean canJump = true;
-        
-        for(int i = 0; i < nums.length ; i++){
-            if (nums[i] >= nums.length - i - 1) {
-                break;
-            } else {
-                int maxStep = 0;
-                int target = 0;
-                for (int j = 1 ; j <= nums[i] ; j++ ) {
-                    if (nums[i+j] >= maxStep) {
-                        maxStep = nums[i+j];
-                        target = i + j -1;
-                    }
-                }
-                
-                if (maxStep == 0 ) {
-                    canJump = false;
-                    break;
-                } else {
-
-                    i = target;
-                }
-
-
-            }
-        }
-                return canJump;   
+        return false;
     }
 }
