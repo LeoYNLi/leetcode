16f464e9df3878e0966c953e4a3879c58d694816
Yiunam LI <liyiunam37@gmail.com>
2026-01-24 20:32:52 +0800

    added m 238

---
+++ leetcode_java/app/src/test/java/org/example/app/TestM238.java
@@ -0,0 +1,24 @@
+package org.example.app;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import org.junit.jupiter.api.Test;
+
+public class TestM238 {
+    @Test
+    void case1() {
+        Medium238 inst = new Medium238();
+        int[] nums = { 1, 2, 3, 4 };
+        int[] res = inst.productExceptSelf(nums);
+        int[] expect = { 24, 12, 8, 6 };
+        assertArrayEquals(expect, res);
+    }
+
+    @Test
+    void case2() {
+        Medium238 inst = new Medium238();
+        int[] nums = { -1, 1, 0, -3, 3 };
+        int[] res = inst.productExceptSelf(nums);
+        int[] expect = { 0, 0, 9, 0, 0 };
+        assertArrayEquals(expect, res);
+    }
+}
