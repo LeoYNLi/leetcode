8af04539fd2a8b06a7d0d933f962f01f9fea056a
Yiunam LI <liyiunam37@gmail.com>
2026-02-01 12:42:32 +0800

    added e 14

---
+++ leetcode_java/app/src/main/java/org/example/app/Easy14.java
@@ -0,0 +1,7 @@
+package org.example.app;
+
+public class Easy14 {
+    public String longestCommonPrefix(String[] strs) {
+        return "";
+    }
+}
---
+++ leetcode_java/app/src/test/java/org/example/app/TestE14.java
@@ -0,0 +1,39 @@
+package org.example.app;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestE14 {
+    @Test
+    void case1() {
+        String[] strs = { "flower", "flow", "flight" };
+        Easy14 inst = new Easy14();
+        String res = inst.longestCommonPrefix(strs);
+        assertEquals("fl", res);
+    }
+
+    @Test
+    void case2() {
+        String[] strs = { "dog", "racecar", "car" };
+        Easy14 inst = new Easy14();
+        String res = inst.longestCommonPrefix(strs);
+        assertEquals("", res);
+    }
+
+    @Test
+    void case3() {
+        String[] strs = { "a", "b", };
+        Easy14 inst = new Easy14();
+        String res = inst.longestCommonPrefix(strs);
+        assertEquals("", res);
+    }
+
+    @Test
+    void case4() {
+        String[] strs = { "ab", "a", };
+        Easy14 inst = new Easy14();
+        String res = inst.longestCommonPrefix(strs);
+        assertEquals("a", res);
+    }
+}
