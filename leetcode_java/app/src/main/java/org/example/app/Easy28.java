cff07ad12144d6ec30327ea68db6c41630a3df3e
Leo <liyiunam@hotmail.com>
2026-02-03 11:46:48 +0800

    remove ans from e28

--- leetcode_java/app/src/main/java/org/example/app/Easy28.java
+++ leetcode_java/app/src/main/java/org/example/app/Easy28.java
@@ -2,18 +2,6 @@ package org.example.app;
 
 public class Easy28 {
     public int strStr(String haystack, String needle) {
-        for (int i = 0; i < haystack.length(); i++) {
-            if (haystack.charAt(i) == needle.charAt(0)) {
-
-                if (i + needle.length() > haystack.length()) {
-                    return -1;
-                } else {
-                    if (haystack.substring(i, i + needle.length()).equals(needle)) {
-                        return i;
-                    }
-                }
-            }
-        }
         return -1;
     }
 }
