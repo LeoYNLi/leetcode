9bb8920539b063f54a0ee4afcd50a36a7a6ef000
Yiunam LI <liyiunam37@gmail.com>
2026-02-06 16:39:29 +0800

    updated e125

--- leetcode_java/app/src/main/java/org/example/app/Easy125.java
+++ leetcode_java/app/src/main/java/org/example/app/Easy125.java
@@ -2,31 +2,23 @@ package org.example.app;
 
 public class Easy125 {
     public boolean isPalindrome(String s) {
-        if (s.length() == 1) {
-            return true;
+        int start = 0;
+        int last = s.length() - 1;
+        while (start <= last) {
+            char currFirst = s.charAt(start);
+            char currLast = s.charAt(last);
+            if (!Character.isLetterOrDigit(currFirst)) {
+                start++;
+            } else if (!Character.isLetterOrDigit(currLast)) {
+                last--;
+            } else {
+                if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+                    return false;
+                }
+                start++;
+                last--;
+            }
         }
-        int l = 0;
-        int r = s.length() - 1;
-
-        while (l < r) {
-            String lChar = String.valueOf(s.charAt(l)).toLowerCase();
-            if (!lChar.matches("[a-zA-Z0-9]")) {
-                l += 1;
-                continue;
-            }
-            String rChar = String.valueOf(s.charAt(r)).toLowerCase();
-            if (!rChar.matches("[a-zA-Z0-9]")) {
-                r -= 1;
-                continue;
-            }
-
-            if (!lChar.equals(rChar)) {
-                return false;
-            }
-            l += 1;
-            r -= 1;
-        }
-
         return true;
     }
 }
