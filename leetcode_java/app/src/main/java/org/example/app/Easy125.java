2632617242e798ec1daa9a06881c899f8ad83ae6
Yiunam LI <liyiunam37@gmail.com>
2026-02-07 15:42:48 +0800

    remove ans from e125

--- leetcode_java/app/src/main/java/org/example/app/Easy125.java
+++ leetcode_java/app/src/main/java/org/example/app/Easy125.java
@@ -2,23 +2,7 @@ package org.example.app;
 
 public class Easy125 {
     public boolean isPalindrome(String s) {
-        int start = 0;
-        int last = s.length() - 1;
-        while (start <= last) {
-            char currFirst = s.charAt(start);
-            char currLast = s.charAt(last);
-            if (!Character.isLetterOrDigit(currFirst)) {
-                start++;
-            } else if (!Character.isLetterOrDigit(currLast)) {
-                last--;
-            } else {
-                if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
-                    return false;
-                }
-                start++;
-                last--;
-            }
-        }
+
         return true;
     }
 }
