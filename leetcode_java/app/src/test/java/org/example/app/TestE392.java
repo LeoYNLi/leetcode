215174a7ec25ae0a946b7c13657382a1ca05ad2e
Yiunam LI <liyiunam37@gmail.com>
2026-02-07 20:53:47 +0800

    added e392

---
+++ leetcode_java/app/src/test/java/org/example/app/TestE392.java
@@ -0,0 +1,52 @@
+package org.example.app;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestE392 {
+    @Test
+    void case1() {
+        String s = "abc";
+        String t = "ahbgdc";
+        Easy392 inst = new Easy392();
+        boolean res = inst.isSubsequence(s, t);
+        assertEquals(true, res);
+    }
+
+    @Test
+    void case2() {
+        String s = "axc";
+        String t = "ahbgdc";
+        Easy392 inst = new Easy392();
+        boolean res = inst.isSubsequence(s, t);
+        assertEquals(false, res);
+    }
+
+    @Test
+    void case3() {
+        String s = "b";
+        String t = "c";
+        Easy392 inst = new Easy392();
+        boolean res = inst.isSubsequence(s, t);
+        assertEquals(false, res);
+    }
+
+    @Test
+    void case4() {
+        String s = "";
+        String t = "ahbgdc";
+        Easy392 inst = new Easy392();
+        boolean res = inst.isSubsequence(s, t);
+        assertEquals(true, res);
+    }
+
+    @Test
+    void case5() {
+        String s = "acb";
+        String t = "ahbgdc";
+        Easy392 inst = new Easy392();
+        boolean res = inst.isSubsequence(s, t);
+        assertEquals(false, res);
+    }
+}
