c0343a7a1d3ac5d6174052f663e754e0d4bbec40
Yiunam LI <liyiunam37@gmail.com>
2026-02-08 15:10:04 +0800

    update m 167

---
+++ leetcode_java/app/src/main/java/org/example/app/Medium167.java
@@ -0,0 +1,7 @@
+package org.example.app;
+
+public class Medium167 {
+    public int[] twoSum(int[] numbers, int target) {
+        return new int[0];
+    }
+}
---
+++ leetcode_java/app/src/test/java/org/example/app/TestM167.java
@@ -0,0 +1,18 @@
+package org.example.app;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestM167 {
+    @Test
+    void case1() {
+        int[] numbers = new int[] { 2, 7, 11, 15 };
+        int target = 9;
+        Medium167 inst = new Medium167();
+        int[] res = inst.twoSum(numbers, target);
+        int[] expected = { 1, 2 };
+        assertArrayEquals(expected, res);
+
+    }
+}
