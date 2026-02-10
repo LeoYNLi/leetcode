49e5025c968366b1d3a2197a9962d478c156d7dc
Leo <liyiunam@hotmail.com>
2026-02-10 14:22:17 +0800

    completed

---
+++ leetcode_java/app/src/main/java/org/example/app/Medium15.java
@@ -0,0 +1,12 @@
+package org.example.app;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Medium15 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        return res;
+    }
+}
---
+++ leetcode_java/app/src/test/java/org/example/app/TestM15.java
@@ -0,0 +1,49 @@
+package org.example.app;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class TestM15 {
+    @Test
+    void case1() {
+        int[] nums = { -1, 0, 1, 2, -1, -4 };
+        List<List<Integer>> expected = new ArrayList<>();
+        expected.add(Arrays.asList(-1, -1, 2));
+        expected.add(Arrays.asList(-1, 0, 1));
+
+        Medium15 inst = new Medium15();
+        List<List<Integer>> res = inst.threeSum(nums);
+        assertEquals(expected, res);
+
+    }
+
+    @Test
+    void case2() {
+        int[] nums = { 0, 0, 0, 0 };
+        List<List<Integer>> expected = new ArrayList<>();
+        expected.add(Arrays.asList(0, 0, 0));
+
+        Medium15 inst = new Medium15();
+        List<List<Integer>> res = inst.threeSum(nums);
+        assertEquals(expected, res);
+
+    }
+
+    @Test
+    void case3() {
+        int[] nums = { 0, 0, 0 };
+        List<List<Integer>> expected = new ArrayList<>();
+        expected.add(Arrays.asList(0, 0, 0));
+
+        Medium15 inst = new Medium15();
+        List<List<Integer>> res = inst.threeSum(nums);
+        assertEquals(expected, res);
+
+    }
+}
