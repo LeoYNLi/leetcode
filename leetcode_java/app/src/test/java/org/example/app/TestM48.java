180eb55cdc8d86c130d11d228f467bf7de1463d1
Yiunam LI <liyiunam37@gmail.com>
2026-03-21 20:56:38 +0800

    updated test m 48

--- leetcode_java/app/src/test/java/org/example/app/TestM48.java
+++ leetcode_java/app/src/test/java/org/example/app/TestM48.java
@@ -1,5 +1,7 @@
 package org.example.app;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 import org.example.app.matrix.Medium48;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +11,8 @@ public class TestM48 {
         int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
         int[][] expect = { { 7, 4, 1 }, { 8, 5, 2 }, { 9, 6, 3 } };
         Medium48 inst = new Medium48();
-        inst.rotate(null);
+        inst.rotate(matrix);
+        assertArrayEquals(matrix, expect);
     }
 
     @Test
@@ -17,6 +20,7 @@ public class TestM48 {
         int[][] matrix = { { 5, 1, 9, 11 }, { 2, 4, 8, 10 }, { 13, 3, 6, 7 }, { 15, 14, 12, 16 } };
         int[][] expect = { { 15, 13, 2, 5 }, { 14, 3, 4, 1 }, { 12, 6, 8, 9 }, { 16, 7, 10, 11 } };
         Medium48 inst = new Medium48();
-        inst.rotate(null);
+        inst.rotate(matrix);
+        assertArrayEquals(matrix, expect);
     }
 }
