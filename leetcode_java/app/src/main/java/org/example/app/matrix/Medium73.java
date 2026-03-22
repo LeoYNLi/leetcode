029feb571c699d9e99daad3145309352c74d4032
Yiunam LI <liyiunam37@gmail.com>
2026-03-22 22:44:46 +0800

    clean up m73

--- leetcode_java/app/src/main/java/org/example/app/matrix/Medium73.java
+++ leetcode_java/app/src/main/java/org/example/app/matrix/Medium73.java
@@ -4,28 +4,6 @@ import java.util.HashSet;
 
 public class Medium73 {
     public void setZeroes(int[][] matrix) {
-        HashSet<Integer> zeroCol = new HashSet<>();
-        HashSet<Integer> zeroRow = new HashSet<>();
-
-        for (int r = 0; r < matrix.length; r++) {
-
-            for (int c = 0; c < matrix[0].length; c++) {
-                if (matrix[r][c] == 0) {
-                    zeroCol.add(c);
-                    zeroRow.add(r);
-                }
-            }
-        }
-
-        int[] newRow = new int[matrix[0].length];
-        for (int i = 0; i < matrix.length; i++) {
-            if (zeroRow.contains(i)) {
-                matrix[i] = newRow;
-            }
-            for (int col : zeroCol) {
-                matrix[i][col] = 0;
-            }
-        }
     }
 
 }
